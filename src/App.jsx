e842e40d8ad20b467274b5472f0ea882b834f884
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-01 17:10:40 +0200

    text

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Knowledges from './pages/Knowledges';
 import NotFound from './pages/NotFound';
 import Portfolio from './pages/Portfolio';
 function App() {
-  
+
 
   return (
     <>
-      <h1>hello react</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/competences' element={<Knowledges/>}/>
+        <Route path='*' element={<NotFound/>}/>
+
+      </Routes>
+
     </>
   )
 }
