ee54ea383fc66bb12d0a9367d1ec92a308b6b3c6
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-02 16:30:44 +0200

    coding pages portfolio and component project and projectList

---
+++ src/components/portfolio/ProjectList.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+class ProjectList extends PureComponent {
+   
+    render() {
+        return (
+            <div className="">
+                project
+            </div>
+        )
+    }
+}
+
+export default ProjectList
