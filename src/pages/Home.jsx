adfcbfa8ea600635d9460be6f05102ea0cd02134
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-03 15:31:56 +0200

    debug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,22 @@ const Home = () => {
         <div className='home'>
             <Navigation />
             <div className="homeContent">
-                <div className="content">
-                    <h1>Boris code</h1>
-                    <h2>Developpeur full stack</h2><br /><b><br /></b>
+                <img  className="responsive-img" src="../media/boriscode.jpg" alt="boris code" style={{
+                    height: '200px',
+                    width: '200px',
+                    borderRadius: '50%',
+                    objectFit: 'cover',
+                    position: 'absolute',
+                    top: 0,
+                    left: '30%',
+                    bottom: '30px',
+                }} />
+                <div className="content"><br /><br /><br/><br />
+                    <h1 >Boris Simo </h1>
+                    {/* <h1>Boris code</h1> */}
+                    <h2>Developpeur web</h2><br />
                     <div className="pdf">
-                        <a href="./media/CV2_complet_boris.pdf" download  target='_blank '>Telecharger cv</a>
+                        <a href="./media/CV2_complet_boris.pdf" download target='_blank '>Telecharger cv</a>
                     </div>
                 </div>
             </div>
