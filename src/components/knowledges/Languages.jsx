8889979cf2ddcc2a04eb809f0d92a520fde54049
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-03 17:54:41 +0200

    text

--- src/components/knowledges/Languages.jsx
+++ src/components/knowledges/Languages.jsx
@@ -3,16 +3,17 @@ import ProgressBar from './ProgressBar'
 class Languages extends Component {
     state = {
         languages: [
-            { id: 1, value: 'Javascript', xp: '1.8' },
-            { id: 2, value: 'Css', xp: '2' },
-            { id: 3, value: 'Php', xp: '0.7' },
-            { id: 4, value: 'Python', xp: '0.4' }
+            { id: 1, value: 'Javascript', xp: '1' },
+            { id: 2, value: 'Css', xp: '1.5' },
+            { id: 3, value: 'Php', xp: '1.8' }
+            // { id: 4, value: 'Python', xp: '0.4' }
 
         ],
         frameworks: [
             { id: 1, value: 'React', xp: '1.4' },
             { id: 2, value: 'Tailwind Css', xp: '1.5' },
             { id: 3, value: 'Laravel', xp: '1.5' },
+            { id: 3, value: 'Wordpress', xp: '0.5' },
             { id: 4, value: 'Express', xp: '0.6' }
         ]
 
