8cb5ebdf340d001e3b6349d5e34e22016e61d9e8
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-19 15:39:00 +0200

    change classico

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-
+import Contact from '../pages/Contact';
+import Portfolio from '../pages/Portfolio';
+import Knowledges from '../pages/Knowledges';
+import NotFound from '../pages/NotFound';
 
 const Home = () => {
     return (
@@ -29,7 +32,12 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <Knowledges />
+            <Portfolio />
+            <Contact />
+
         </section>
+
     );
 };
 
