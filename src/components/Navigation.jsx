b8b131550965fd79effb96843a281e0cb8906c7e
boris2442 <aubinborissimotsebo@gmail.com>
2025-07-28 17:34:16 +0200

    change picture

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,7 +10,7 @@ const Navigation = () => {
             <div className="sidebar">
                 <div className="id">
                     <div className="idContent">
-                        <img src="./media/boriscode.jpg" alt="profil de boris code" /><br/>
+                        <img src="./media/boris-code-237.jpg" alt="profil de boris aubin Simo Tsebo" /><br/>
                         <h3>AUBIN SIMO</h3>
                     </div>
                 </div>
