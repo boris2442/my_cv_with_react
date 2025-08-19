f61e22903a237b9add117d3ea7a19ecb37af649c
boris2442 <aubinborissimotsebo@gmail.com>
2025-08-19 13:05:51 +0200

    add campuspay same experience

--- src/components/knowledges/Experience.jsx
+++ src/components/knowledges/Experience.jsx
@@ -4,17 +4,29 @@ const Experience = () => {
     return (
         <div className='experience'>
             <h3>Experiences</h3>
-            <div className="exp-1">
-                <h4>Chef de projet evendeco.com pour l'entreprise SUPER DECOR</h4>
+            <div className="exp-1" >
+                <h4 style={{color:'#10182A'}}>Chef de projet evendeco.com pour l'entreprise SUPER DECOR</h4>
                 <h5>2024 - 2025</h5>
                 <p>Evendeco est une entreprise camerounaise spécialisée dans la décoration événementielle. Elle conçoit et réalise des ambiances uniques et personnalisées pour tout type d’événement (mariage, entreprise, privé), avec un souci d’élégance, de créativité et de satisfaction client.</p>
             </div>
             <div className="exp-2">
-                <h4>Directeur Technique && chef de projet life.com</h4>
+                <h4 style={{color:'#10182A'}}>Directeur Technique && chef de projet life.com</h4>
                 <h5>2024 - 2025</h5>
                 <p>Life est une entreprise camerounaise engagée dans la fabrication, la purification et la commercialisation d’eau potable. Sa mission principale est de fournir une eau de qualité, accessible et saine aux populations du Cameroun, et à terme, de contribuer à l'amélioration des conditions de vie à travers l’Afrique.
 
                     Basée sur des standards élevés de qualité et de durabilité, Life adopte des procédés modernes de traitement d’eau pour garantir la pureté, le goût et la sécurité sanitaire de ses produits. Elle ambitionne de devenir une référence dans le domaine de l’eau embouteillée en Afrique, tout en intégrant des pratiques respectueuses de l’environnement et en favorisant le développement local.</p>
+            </div>
+
+            <div className="exp-3">
+                <h4 style={{color:'#10182A'}}>Développeur Full-Stack && Chef de projet CAMPUSPAY</h4>
+                <h5>2024 - 2025</h5>
+                <p>CampusPay est une plateforme numérique conçue pour simplifier et sécuriser la gestion des paiements académiques dans les établissements scolaires et universitaires.
+                    Elle permet aux étudiants, parents et institutions de réaliser et de suivre les transactions financières de manière rapide, transparente et fiable.
+
+                    En tant que développeur Full-Stack et chef de projet, j’ai participé à la conception, au développement et au déploiement de la solution.
+                    J’ai notamment travaillé sur l’architecture technique, l’intégration des moyens de paiement (Orange Money, MTN Mobile Money, PayPal), la gestion des utilisateurs et la mise en place d’un tableau de bord interactif pour l’administration.
+
+                    Ce projet illustre ma capacité à allier compétences techniques et vision produit pour répondre à des problématiques concrètes de digitalisation et de transformation numérique dans le secteur éducatif.</p>
             </div>
         </div>
     );
